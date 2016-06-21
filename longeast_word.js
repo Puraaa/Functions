d62db6fa67f30baea58d1f71d6e872b1100c44e0
Puraaa <pqinganaaa@gmail.com>
2016-06-21 14:25:55 +0200

    modify longest word

---
+++ longeast_word.js
@@ -0,0 +1,12 @@
+function longestWord(hello) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+    str.forEach(function(str) {
+        if (longest < str.length) {
+            longest = str.length;
+            word = str;
+        }
+    });
+    return word;
+};
---
+++ longest_word_test.js
@@ -0,0 +1,4 @@
+QUnit.test( "longest_word_test", function( assert ) {
+	var result = longestWord("hello");
+  assert.ok( result, "Passed!" );
+});
